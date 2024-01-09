1ae8bb7b5aa03e6fc43240a00c1ef8818dea3d73
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-01-09 20:17:33 -0300

    chore: update code response

--- src/CreateUser.API/Model/Response/ViewResponse.cs
+++ src/CreateUser.API/Model/Response/ViewResponse.cs
@@ -8,11 +8,11 @@ public class ViewResponse
     public ViewResponse(object data, short code = 201) =>
         (Data, Code) = (data, code);
     
-    public short Code { get; set; } = 202;
+    public short Code { get; set; } = 201;
     public List<string> Erros { get; set; } = new ();
     public object Data { get; set; } = null!;    
 
-    public void AddData(object data, short code = 202) => (Data, Code) = (data, code);
+    public void AddData(object data, short code = 201) => (Data, Code) = (data, code);
     public void AddErros(List<string> erros, short code = 404) => (Erros, Code) = (erros, code);
     public void AddErros(string erro, short code = 404)
     {
